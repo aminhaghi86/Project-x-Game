c58a418cfea3746312264a0660a5c596a7a6fcfc
Maximiliam Reimertz <97292293+mxrmrtz@users.noreply.github.com>
2022-11-24 16:25:28 +0100

    scoreboard
    Connected firebase to the DoM

--- page4/page4.js
+++ page4/page4.js
@@ -1,31 +1,43 @@
-"use strict";
 const firebaseConfig = {
-  apiKey: "AIzaSyASTKEqpfcsrVt-YYuvYgj-BrfEAFTeFwM",
-  authDomain: "project-xgame.firebaseapp.com",
-  projectId: "project-xgame",
-  storageBucket: "project-xgame.appspot.com",
-  messagingSenderId: "821288931646",
-  appId: "1:821288931646:web:03b2303116bdb09b501c6f",
-};
+    apiKey: "AIzaSyASTKEqpfcsrVt-YYuvYgj-BrfEAFTeFwM",
+    authDomain: "project-xgame.firebaseapp.com",
+    projectId: "project-xgame",
+    storageBucket: "project-xgame.appspot.com",
+    messagingSenderId: "821288931646",
+    appId: "1:821288931646:web:03b2303116bdb09b501c6f",
+  };
 
-// Initialize Firebase
+const list = document.getElementById('list')
 const app = firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
-const displayFirebaseSection = document.getElementById("displayFirebase");
-async function information() {
-  displayFirebaseSection.innerHTML = "";
-  let datas = await db.collection("userInfo").get();
-  //   console.log(db.collection('userInfo'));
-  console.log(datas.docs);
-  datas.docs.forEach((element) => {
-    displayFirebaseSection.innerHTML = ` 
-     ${element.username}
-     ${element.score}
-    `;
-  });
-}
-information();
+const getUserInfo = async () => {
+    const scores = await db.collection("userInfo").get();
+    renderPosts(scores.docs);
+  };
 
-// `
+  db.collection("userInfo")
+  .orderBy("userInfo", "desc")
+  .onSnapshot((snapshot) => {
+    renderPosts(snapshot.docs);
+  });
+
+
+  const renderPosts = (scores) => {
+    list.innerHTML = "";
+  
+    for (let score of scores) {
+      const data = score.data();
+  
+      const listEl = document.createElement("div");
+      listEl.innerHTML = `
+          ${data.score} 
+          ${data.username} 
+  
+      `;
+      list.append(listEl);
+    }
+  };
+
+  getUserInfo()
