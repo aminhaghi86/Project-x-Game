73e5a7d03fac24022823b2cc5ae37201589860d3
Maximiliam Reimertz <97292293+mxrmrtz@users.noreply.github.com>
2022-11-25 13:18:48 +0100

    Page 2 Update
    Created door switching and game folders

---
+++ page2/page2.js
@@ -0,0 +1,41 @@
+const doors = document.querySelectorAll(".doors");
+const backdrop = document.getElementById("backdrop");
+const doorSound = document.getElementById("door-sound");
+const wrongAnswer = document.getElementById("wrong-answer");
+const exitGame = document.getElementById("exit-game");
+
+// variables
+let randomNum = Math.floor(Math.random() * 3);
+
+// variables
+
+// door opening sound
+
+doors.forEach((door) => {
+  door.onclick = () => {
+    doorSound.play();
+    setTimeout(() => {
+      window.location.href = `../game${randomNum}/game${randomNum}.html`;
+    }, 1500);
+  };
+});
+// door opening sound
+
+const falseDoor = () => {
+  selectDoor = doors[randomNum];
+  console.log(selectDoor);
+  selectDoor.src = `../images/door-stage-2.png`;
+  selectDoor.onclick = () => {
+    doorSound.play();
+    setTimeout(() => {
+      wrongAnswer.style.animationPlayState = "running";
+      wrongAnswer.classList.remove("inactive");
+      backdrop.classList.remove("inactive");
+    }, 1500);
+  };
+};
+falseDoor();
+
+exitGame.onclick = () => {
+  window.location.href = "../index.html";
+};
