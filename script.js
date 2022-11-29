9ea02749cd7a2e193539aa91da9b13affd1e184e
amin <amin.haghi@hyperisland.se>
2022-11-29 02:09:08 +0100

    changed doors from 3 to 4 doors+ form validatation

--- script.js
+++ script.js
@@ -6,81 +6,37 @@ const noBtn = document.getElementById("noBtn");
 
 form.onsubmit = function (e) {
   e.preventDefault();
+  const inputName = document.getElementById("name").value;
+  inputName !== null && inputName !== "" ? saveData(inputName) : errorMessage();
 
   //
-  const backDropEl = document.getElementById("backdrop");
+};
 
+//save data
+const saveData = (inputName) => {
+  setName(inputName);
+  setScore(0);
   setTimeout(() => {
     window.location.href = "../page1/page1.html";
   }, 500);
-
-  const inputName = document.getElementById("name").value;
-  setName(inputName);
-  setScore(0);
 };
+//end of save data
 
-// AUDIO play OnLoad or OnClick
-// window.addEventListener("load",function(){
-//   var player1 = document.getElementById("player");
-//   player1.play();
-//  })
-
-//TEXT in chatbox appears
-function timedText() {
-  const element = document.getElementById("p1");
-  element.innerHTML = "";
-
+// Error Message
+const errorMessage = () => {
   setTimeout(() => {
-    element.innerHTML = "You. Are. FIRED!";
-  }, 1000);
+    const funnyImage = document.createElement("img");
+    funnyImage.setAttribute("class", "funnycat");
+    funnyImage.setAttribute("src", "../images/catfunny.gif");
+    document.body.appendChild(funnyImage);
+  }, 500);
   setTimeout(() => {
-    element.innerHTML = "*cries in dog*";
+    document.querySelectorAll(".funnycat").forEach((element) => {
+      element.style.display = "none";
+    });
+  }, 2500);
+  setTimeout(() => {
+    alert("please enter your name :)");
   }, 3000);
-  setTimeout(() => {
-    element.innerHTML = "NEEEEXT!";
-  }, 4000);
-  setTimeout(() => {
-    element.innerHTML =
-      "We here at Dogs of Dogstreet are looking for new talents to recruit! Do you think you have what it takes to make it to the top of our CATdidate list?";
-  }, 6000);
-}
-timedText();
-
-//CAT APPEARS
-let catPicture = document.getElementById("catPicture");
-
-window.setTimeout(function () {
-  catPicture.classList.add("visible");
-}, 8900);
-
-//_____YES/NO BTNS APPEARS
-let chatBoxBtns = document.getElementById("chatBoxBtns");
-
-window.setTimeout(function () {
-  chatBoxBtns.classList.add("display");
-}, 6000);
-
-//___CHATBOX APPEARS
-let chatBox = document.getElementById("chatBoxText");
-
-window.setTimeout(function () {
-  chatBox.classList.add("visible");
-}, 1000);
-
-// NO-btn reveals 'Believe in yourself' Div and plays noBtn-Audio
-function revealBossChatbox() {
-  let playNo = function () {
-    document.getElementById("audioNo").play();
-  };
-  playNo();
-  let x = document.getElementById("bossChatbox");
-  x.classList.remove("inactive");
-}
-noBtn.onclick = () => {
-  revealBossChatbox();
 };
-
-// YES-btn-audio
-let playYes = function () {
-  document.getElementById("audioYes").play();
-};
+// End OF Error Message
